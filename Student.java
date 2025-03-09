453cb13759c7708b07f8808cc1f4cda59f9a430c
csug-ko <katherine.oconnor@csuglobal.edu>
2025-03-09 21:31:37 +0000

    formatting structure update
    includes formatting structure, class description, file description and copy write notice

--- Student.java
+++ Student.java
@@ -1,4 +1,24 @@
+/***************************************************************************************************
+ * Student.java
+ * Katie | Katherine O'Connor
+ * Create a Student object that has a name, address and gpa.
+ * Version 1.0
+ *                         Copywrite Notice : 
+ * This is intellectualy Property of CSUG & other unlisted parties
+ * You do not have the right to read, use, copy, or even have access to this document
+ * Unless submitted to you by the author.
+ * If this document is not intended for you please stop reading and dispose of immediatly 
+ ***************************************************************************************************
+ */
+
+
 package StudentsGrades;
+
+/*
+* Student Class
+* @ Version 1.0
+* @ Katherine O'Connor
+*/
 
 public class Student {
     private String Studentname;
@@ -9,17 +29,32 @@ public class Student {
     Student(){}
 
     // Overriden Constructor
-    Student(String username,String userAddress, double userGPA)
-    {this.Studentname = username;this.GPA = userGPA;this.address = userAddress;}
+    Student(String username,String userAddress, double userGPA){
+        this.Studentname = username;
+        this.GPA = userGPA;
+        this.address = userAddress;
+    }
 
-    // The setters for this Student Object Shell
-    void setName(String userName){this.Studentname = userName;}
-    void setAddress(String userAddress){this.address = userAddress;}
-    void setGPA(double userGPA){this.GPA = userGPA;}
+    // The setters for this Student Clsdd Shell
+    void setName(String userName){
+        this.Studentname = userName;
+    }
+    void setAddress(String userAddress){
+        this.address = userAddress;
+    }
+    void setGPA(double userGPA){
+        this.GPA = userGPA;
+    }
 
-    // The getters for this Student Object Shell
-    String getName(String userName){return this.Studentname;}
-    String getAddress(String userAddress){return this.address;}
-    double getGPA(double userGPA){return this.GPA;}
+    // The getters for this Student Class Shell
+    String getName(String userName){
+        return this.Studentname;
+    }
+    String getAddress(String userAddress){
+        return this.address;
+    }
+    double getGPA(double userGPA){
+        return this.GPA;
+    }
     
 }
